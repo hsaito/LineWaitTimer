22fcee218327227dbf1799bf4bb7583cfae12a83
Hideki Saito <hidekis@gmail.com>
2012-09-04 00:42:22 -0700

    Initial checkin

---
+++ src/com/hclippr/linewaittimer/LineWaitTimerActivity.java
@@ -0,0 +1,13 @@
+package com.hclippr.linewaittimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LineWaitTimerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
